677ca8ab0e82501cc20fc6c533f3d06d60d12d5e
Stephan Kuschel <stephan.kuschel@gmail.com>
2014-07-27 22:06:19 +0200

    pep8 style changes.

--- ipread.py
+++ ipread.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2
 
 '''
-Module providing the IPreader class for reading Image Plates data files. Multiple Files
-can be combined to one single Image. Automatic PSL conversion is applied.
+Module providing the IPreader class for reading Image Plates data files.
+Multiple Files can be combined to one single Image. Automatic PSL
+conversion is applied.
 
 Author: Stephan Kuschel
 '''
@@ -28,14 +29,17 @@ def cnttopsl(cnt, R, S, L):
     converts a count number cnt to PSL using the given values for R, S and L.
     '''
     if ne is None:
-        return (R / 100.) ** 2 * (4000. / S) * 10.**(L * (cnt / 65536.0 - 0.5))
+        return (R / 100.) ** 2 * (4000. / S) * \
+            10. ** (L * (cnt / 65536.0 - 0.5))
     else:
-        return ne.evaluate('(R / 100.) ** 2 * (4000. / S) * 10.**(L * (cnt / 65536.0 - 0.5))')
+        return ne.evaluate('(R / 100.) ** 2 * (4000. / S) * '
+                           '10.**(L * (cnt / 65536.0 - 0.5))')
 
 
 def readimg(filename, rows, cols):
     '''
-    Attempts to read the .img file filename (with or without '.img') assuming it was read with rows rows and cols cols.
+    Attempts to read the .img file filename (with or without '.img')
+    assuming it was read with rows rows and cols cols.
     '''
     dt = np.dtype(np.uint16)
     dt = dt.newbyteorder('>')  # change to big endian
@@ -44,16 +48,20 @@ def readimg(filename, rows, cols):
         ret = np.reshape(np.fromfile(f, dtype=dt), (rows, cols))
     return np.array(ret, dtype=np.float64)
 
+
 # ----- Classes -----
 class Infreader():
 
     def __init__(self, filename):
         '''
-        This class reads a single .inf file and provides informations about the read out settings used.
-        Since the read out settings define the PSL conversion, it also defines a function to calculate
+        This class reads a single .inf file and provides informations about
+        the read out settings used.
+        Since the read out settings define the PSL conversion, it also
+        defines a function to calculate
         the PSL value for a given number of counts at these readout settings.
 
-        filename must be the exact filename of the .inf file _including_ its extension.
+        filename must be the exact filename of the .inf file _including_ its
+        extension.
         '''
         self.filename = filename
         self.name = os.path.basename(filename.strip('\n'))
@@ -69,15 +77,17 @@ class Infreader():
         if self.R != self.R2:
             warnings.warn('The Pixels of the IP picture are no squares.')
 
-
     def topsl(self, c):
         """
-        returns the PSL value corresponding to the count number c at the readout settings defined by this Infreader object.
+        returns the PSL value corresponding to the count number c at the
+        readout settings defined by this Infreader object.
         """
         return cnttopsl(c, self.R, self.S, self.L)
 
     def __str__(self):
-        return '<"' + self.name + '" R:' + str(self.R) + ' cols:' + str(self.cols) + ' rows:' + str(self.rows) + ' S:' + str(self.S) + ' L:' + str(self.L) + '>'
+        return '<"' + self.name + '" R:' + str(self.R) + ' cols:' + \
+               str(self.cols) + ' rows:' + str(self.rows) + ' S:' + \
+               str(self.S) + ' L:' + str(self.L) + '>'
 
     def __eq__(self, other):
         '''
@@ -91,23 +101,29 @@ class IPreader(Infreader):
 
     def __init__(self, *args):
         '''
-        returns an IPreader Object, that allows access to the PSL converted Image 'self.psl'. This is derived from the
-        class Infreader since all combined images MUST have identical read out settings.
+        returns an IPreader Object, that allows access to the PSL converted
+        Image 'self.psl'. This is derived from the class Infreader since all
+        combined images MUST have identical read out settings.
 
         *args can be
-        - a single file with '.img' or '.inf' or no extension. In this case this single file is read and converted.
-        - a single filename that can be extended into multiple filenames by 'glob.glob(filename)'. Those files are then
-          processed as follows:
-        - multiple files of the former type. In this case it is assumed, that all files are multiple readouts
-          of the same image plate in alpha-numerical order. All files are read and the program will try to combine them to a
+        - a single file with '.img' or '.inf' or no extension.
+          In this case this single file is read and converted.
+        - a single filename that can be extended into multiple filenames
+          by 'glob.glob(filename)'. Those files are then processed as follows:
+        - multiple files of the former type. In this case it is assumed,
+          that all files are multiple readouts
+          of the same image plate in alpha-numerical order.
+          All files are read and the program will try to combine them to a
           single image with higher dynamic range.
-            
+
         The returned IPreader object ip will have the following attributes:
-        - self.scalefactors - list of floats each readout need to be multiplied by
-        - self.scalefactorsstd - list of floats of standard deviation of scalefactors
+        - self.scalefactors - list of floats each readout needs to be
+          multiplied by
+        - self.scalefactorsstd - list of floats of standard deviation
+          of scalefactors
         - self.psls - list of numpy arrays each holding one image
-        - self.psl - the high dynamic range image created by combining the images in self.psls using self.scalefactors
-        
+        - self.psl - the high dynamic range image created by combining
+          the images in self.psls using self.scalefactors
         '''
         if len(args) == 1:
             self.files = glob.glob(args[0])
@@ -115,15 +131,19 @@ class IPreader(Infreader):
             self.files = args
         self.files = [f.strip('.img') for f in self.files]
         self.files = [f.strip('.inf') for f in self.files]
-        self.files = list(set(self.files))  # make list unique, so every file is only read once if name* results in a list containing .img and .inf files.
+        # make list unique, so every file is only read once if
+        # name* results in a list containing .img and .inf files.
+        self.files = list(set(self.files))
         self.files.sort()
 
         Infreader.__init__(self, self.files[0] + '.inf')
         for f in self.files:
             if not self == Infreader(f + '.inf'):
-                raise Exception('File "' + f + '" was read using different read out settings. Refusing HDR assembly.')
+                raise Exception('File "' + f + '" was read using different '
+                                'read out settings. Refusing HDR assembly.')
 
-        self.psls = [self.topsl(readimg(datei, self.rows, self.cols)) for datei in self.files]
+        self.psls = [self.topsl(readimg(datei, self.rows, self.cols))
+                     for datei in self.files]
         # Combine psl pictures to a single HDR picture
         pslsaturate = self.topsl(65525.0)
         pslminimum = pslsaturate / 4.0
@@ -135,14 +155,19 @@ class IPreader(Infreader):
         for n in xrange(1, len(self.psls)):
             picnext = copy.copy(self.psls[n])
             if ne is None:
-                picnext[(picnext > pslsaturate) | (picnext < pslminimum)] = np.nan
+                indizes = (picnext > pslsaturate) | (picnext < pslminimum)
+
             else:
-                picnext[ne.evaluate('(picnext > pslsaturate) | (picnext < pslminimum)')] = np.nan
+                indizes = ne.evaluate('(picnext > pslsaturate) | '
+                                      '(picnext < pslminimum)')
+            picnext[indizes] = np.nan
             A = piclast / picnext
             meand = np.median(A[np.isfinite(A)])
             varianzd = np.var(A[np.isfinite(A)])
             mean = self.scalefactors[n - 1] * meand
-            varianz = self.scalefactors[n - 1] ** 2 * varianzd + sfvar[n - 1] * meand ** 2 + varianzd ** 2 * sfvar[n - 1] ** 2
+            varianz = self.scalefactors[n - 1] ** 2 * varianzd + \
+                sfvar[n - 1] * meand ** 2 + \
+                varianzd ** 2 * sfvar[n - 1] ** 2
             self.scalefactors = np.append(self.scalefactors, mean)
             sfvar = np.append(sfvar, varianz)
             piclast = picnext
@@ -150,9 +175,16 @@ class IPreader(Infreader):
 
         # check consistency
         if not all(self.scalefactors == sorted(self.scalefactors)):
-            warnings.warn('IP Files were not given is ascending read out order! This reduces quality of the assembled picture dramatically or makes it even impossible!')
+            warnings.warn('IP Files were not given is ascending read out '
+                          'order! '
+                          'This reduces quality of the assembled picture '
+                          'dramatically or makes it even impossible!')
         if any(self.scalefactors < 1):
-            warnings.warn('PSL SCALE HAS CHANGED because the first file is always forced to have the scalefactor 1, but first file is NOT the first readout of the IP (in other words: there is at least one scalefactor < 1).')
+            warnings.warn('PSL SCALE HAS CHANGED because the first file is '
+                          'always forced to have the scalefactor 1, but first '
+                          'file is NOT the first readout of the IP '
+                          '(in other words: there is at least one'
+                          'scalefactor < 1).')
 
         # Assemble HDR Image in PSL scale
         self.psl = np.zeros(piclast.shape)
@@ -166,7 +198,6 @@ class IPreader(Infreader):
             count += (picn > 0)
         self.psl /= count
 
-
     def __str__(self):
         return '<"' + str(self.files) + '" R:' + str(self.R) + ' cols:' + str(self.cols) + ' rows:' + str(self.rows) \
             + ' S:' + str(self.S) + ' L:' + str(self.L) + '\n' \
@@ -178,13 +209,25 @@ class IPreader(Infreader):
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description='Previews the Image Plate readout(s) using matplotlib.')
-    parser.add_argument('file', nargs='+', help='input file(s) - can be *.inf or *.img or without extension.')
-    parser.add_argument('-l', help='list properties of assembled image, dont create any plots. No matplotlib is needed.', action='store_true')
-    parser.add_argument('--log', help='creates a log10 plot instead of a linear one.', action='store_true')
-    parser.add_argument('-s', nargs='?', metavar='filename', dest='save', help='save picture of data using matplotlib. If this is given, no interactive window will appear. filename will be auto-generated if omitted.', default='')
+    parser = argparse.ArgumentParser(description='Previews the Image Plate'
+                                     'readout(s) using matplotlib.')
+    parser.add_argument('file', nargs='+',
+                        help='input file(s) - can be *.inf or *.img or '
+                        'without extension.')
+    parser.add_argument('-l',
+                        help='list properties of assembled image, dont '
+                        'create any plots. No matplotlib is needed.',
+                        action='store_true')
+    parser.add_argument('--log',
+                        help='creates a log10 plot instead of a linear one.',
+                        action='store_true')
+    parser.add_argument('-s', nargs='?', metavar='filename', dest='save',
+                        help='save picture of data using matplotlib. '
+                        'If this is given, no interactive window will appear.'
+                        ' filename will be auto-generated if omitted.',
+                        default='')
     args = parser.parse_args()
-    if args.save == None:
+    if args.save is None:
         args.save = args.file[0] + '.png'
     elif args.save == '':
         args.save = None
--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 from setuptools import setup
 from ipread import __version__
 import os
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='ipread',
-    version=__version__,
-	author='Stephan Kuschel',
-	author_email='stephan.kuschel@gmail.com',
-	description='Reads Imageplate files and combines multiple readouts to a single HDR if necessary. Can be used as module or with "python -m IPread" to have a CLI tool for fast preview.',
-	long_description=read('README.md'),
-    url='https://github.com/skuschel/IPread',
-    py_modules=['ipread'],
-	install_requires=['matplotlib', 'numpy']
-	)
+      version=__version__,
+      author='Stephan Kuschel',
+      author_email='stephan.kuschel@gmail.com',
+      description='Reads Imageplate files and combines multiple readouts to '
+                  'a single HDR if necessary. Can be used as module or '
+                  'with "python -m IPread" to have a CLI tool for '
+                  'fast preview.',
+      long_description=read('README.md'),
+      url='https://github.com/skuschel/IPread',
+      py_modules=['ipread'],
+      install_requires=['matplotlib', 'numpy']
+      )
